a75bae4ecbd56cf8f4ae5d4056182170daaffa4a
ToKYo4407 <zawar_ia@mt.iitr.ac.in>
2025-06-25 23:02:05 +0530

    file uploaded

---
+++ helloworld.cpp
@@ -0,0 +1,41 @@
+/*
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=5;
+    cout<<"Hello World"<<endl;
+    cout<<"Size of a is:"<<sizeof(a)<<endl;
+}
+*/
+
+/*#include<iostream>
+using namespace std;
+int main(){
+    //typecasting//changing one  datatype into another
+    int a= 'a';
+    cout<<a<<endl;
+
+    char ch=98;
+    cout<<ch<<endl;
+
+    char ch1=123456;
+    cout<<ch1<<endl;
+   
+}*/
+
+//operators
+#include<iostream>
+using namespace std;
+int main(){
+    int a=2;
+    int b=3;
+
+    bool first=(a==b);
+    cout<<first<<endl;
+
+    bool second=(a<b);
+    cout<<second<<endl;
+
+}
+
